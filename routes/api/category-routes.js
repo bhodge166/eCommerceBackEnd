ced00bf3af72854ade4c0bdbe86b12472e6ba3dd
Brad Hodge <bhodge166@gmail.com>
2022-03-19 16:38:19 -0600

    Uploading initial files with intial routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,81 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", async (req, res) => {
+  try {
+    const CategoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const CategoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const CategoryData = await Category.create(req.body);
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const CategoryData = await Category.update({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const CategoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
